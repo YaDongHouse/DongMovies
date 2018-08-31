8e7ee8ee3bb956b9a11328e0c57a31e3f90c8862
yadong.qiu <yadong.qiu@sao.so>
2018-08-31 18:14:51 +0800

    dialog初识

--- App.js
+++ App.js
@@ -7,12 +7,13 @@
  */
 
 import React, {Component} from 'react';
-import {View,StatusBar} from 'react-native';
+import {View, StatusBar,TouchableOpacity,Text} from 'react-native';
 import MovieView from "./DongMovies/Movies/MovieView";
 import DongDialog from "./DongMovies/Common/DongDialog";
 import DongMenu from "./DongMovies/Common/DongMenu";
 import MovieList from "./DongMovies/Movies/MovieList";
-import {DrawerNavigator,TabNavigator,StackNavigator} from 'react-navigation'
+import {DrawerNavigator, TabNavigator, StackNavigator} from 'react-navigation'
+import ErrorDialog from "./DongMovies/Movies/ErrorDialog";
 
 const RootStack = StackNavigator(
     {//定义路由
@@ -25,16 +26,39 @@ const RootStack = StackNavigator(
     },
     {//定义配置
         initialRouteName: 'MovieList',     //设置初始路由为Home
-        navigationOptions:{
-            header:null
+        navigationOptions: {
+            header: null
         },
     }
 );
 export default class App extends Component<Props> {
+
+
+    _onPress = () =>{
+        this.dialog._show()
+    }
+
     render() {
         return (
-            <RootStack/>
+            <View style={{flex:1}}>
+                <DongDialog/>
+            </View>
+
         );
     }
 }
+
+
+// {/*<View style={{flex:1,alignItems:'center',justifyContent:'center'}}>*/}
+// {/*<TouchableOpacity onPress={this._onPress}>*/}
+// {/*<Text>点击</Text>*/}
+// {/*</TouchableOpacity>*/}
+// {/*<ErrorDialog*/}
+// {/*ref={(popupDialog) => {*/}
+// {/*this.dialog = popupDialog;*/}
+// {/*}}*/}
+// {/*btnName={'按钮'}*/}
+// {/*content={'无此资源'}*/}
+// {/*/>*/}
+// {/*</View>*/}
 
--- DongMovies/Movies/ErrorDialog.js
+++ DongMovies/Movies/ErrorDialog.js
@@ -8,22 +8,54 @@ import PopupDialog, {
     FadeAnimation,
 } from 'react-native-popup-dialog';
 const fadeAnimation = new FadeAnimation({ animationDuration: 150 });
-
+const scaleAnimation = new ScaleAnimation();
 export default class ErrorDialog extends Component {
-    render(){
-        return(<View style={{flex:1}}>
-            <PopupDialog
-                ref={(popupDialog) => {
-                    this.dialog = popupDialog;
-                }}
-                dialogAnimation={fadeAnimation}
-                dialogTitle={<DialogTitle title="温馨提示" />}
-            >
-                <View>
-                    <Text></Text>
-                </View>
 
-            </PopupDialog>
+    _show(){
+        this.dialog.show();
+    }
+
+    render(){
+        return(<View style={{position:'absolute', top:0,bottom:0,left:0,right:0}}>
+                <PopupDialog
+                    ref={(popupDialog) => {
+                        this.dialog = popupDialog;
+                    }}
+                    dialogAnimation={scaleAnimation}
+                    dialogTitle={<DialogTitle style={ss.title} title="温馨提示" />}
+                >
+                    <View style={ss.container}>
+                        <View style={ss.contentContainer}>
+                            <Text>{this.props.content}</Text>
+                        </View>
+                        <View style={ss.btnContainer}>
+                            <DialogButton
+                                text={this.props.btnName}
+                                onPress={()=>{
+                                    console.log("点击按钮")
+                                }}
+                            />
+                        </View>
+                    </View>
+                </PopupDialog>
         </View>)
     }
 }
+let ss = StyleSheet.create({
+    container:{
+        flex:1,
+    },
+    contentContainer:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        padding:5
+    },
+    btnContainer:{
+        backgroundColor:'#0f0'
+    },
+    title:{
+        fontWeight:'bold',
+        fontSize:18,
+    }
+})
